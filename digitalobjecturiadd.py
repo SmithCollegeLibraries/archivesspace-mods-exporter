9efa728a91ec21d1123372e6c23fbaaa9dcd9e67
Claire Marshall <cmarshall@L-west126marsh.local>
2019-01-15 15:04:56 -0500

    Added publish value

--- digitalobjecturiadd.py
+++ digitalobjecturiadd.py
@@ -2,7 +2,7 @@ import json
 import os
 from archivesspace import archivesspace
 import pprint
-from utilities import *
+# from utilities import *
 import argparse
 import logging
 
@@ -61,6 +61,7 @@ def updateDOwithCompassURI(ywca_compass_object):
 	digital_object['file_versions'] = []
 	uri_key = {'file_uri': compass_uri}
 	digital_object['file_versions'].append(uri_key)
+	digital_object['file_versions'][0]['publish'] = True
 	# Updates digital object records with the file URIs
 	response = aspace.post('/repositories/2/digital_objects/' + do_id, requestData=digital_object)
 
